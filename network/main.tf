f9d2d7d3e5dbb16ea9c9a8b4a034f18d2981da9f
vivekgshan <133869257+vivekgshan@users.noreply.github.com>
2025-08-16 19:28:13 +0530

    Create main.tf

---
+++ network/main.tf
@@ -0,0 +1,30 @@
+resource "aws_security_group" "jenkins_sg" {
+  name        = "jenkins_sg"
+  description = "Allow Jenkins traffic"
+  vpc_id      = var.vpc_id
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+output "jenkins_sg_id" {
+  value = aws_security_group.jenkins_sg.id
+}
